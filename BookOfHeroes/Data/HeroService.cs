3eaf6514f77a15039181c363bcbc8a57127e0b6c
Brian Egan <brianjoseph.egan@gmail.com>
2021-04-24 12:58:25 -0700

    Hero Service
    Add in Hero Service with some mock Hero objects. Register the service as a singleton and inject it into the index page. When loading the index page, retrieve the mock Heroes using the service.

---
+++ BookOfHeroes/Data/HeroService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookOfHeroes.Data
+{
+    public class HeroService
+    {
+        private static readonly Hero[] heroes = new Hero[]
+        {
+            new Hero() { Id = 11, Name = "Jaina Proudmoore" },
+            new Hero() { Id = 12, Name = "Rexxar" },
+            new Hero() { Id = 13, Name = "Garrosh Hellscream" },
+            new Hero() { Id = 14, Name = "Uther Lightbringer" },
+            new Hero() { Id = 15, Name = "Anduin Wrynn" },
+            new Hero() { Id = 16, Name = "Valeera Sanguinar" },
+            new Hero() { Id = 17, Name = "Thrall" },
+            new Hero() { Id = 18, Name = "Gul'dan" },
+            new Hero() { Id = 19, Name = "Malfurion Stormrage" },
+            new Hero() { Id = 20, Name = "Illidan Stormrage" },
+        };
+
+        public Task<Hero[]> GetHeroesAsync() => Task.FromResult(heroes);
+    }
+}
--- BookOfHeroes/Startup.cs
+++ BookOfHeroes/Startup.cs
@@ -27,6 +27,7 @@ namespace BookOfHeroes
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
+            services.AddSingleton<HeroService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
